84538c8a8ab660ad19265dd3b70fb8695968c5c8
cnoriega20 <c79.noriega@gmail.com>
2021-03-15 01:20:14 -0500

    Spring initialzr project struture

---
+++ src/main/java/com/springboot/soap/example/SoapbookwebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.soap.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapbookwebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapbookwebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/soap/example/SoapbookwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.soap.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapbookwebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
