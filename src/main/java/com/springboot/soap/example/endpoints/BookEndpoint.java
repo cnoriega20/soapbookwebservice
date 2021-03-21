ba0e8fad89c749094f2ea64d8adbc99c23163637
cnoriega20 <c79.noriega@gmail.com>
2021-03-21 00:47:51 -0500

    Completed BookEndpoint class.

--- src/main/java/com/springboot/soap/example/endpoints/BookEndpoint.java
+++ src/main/java/com/springboot/soap/example/endpoints/BookEndpoint.java
@@ -1,7 +1,12 @@
 package com.springboot.soap.example.endpoints;
 
+import com.springboot.soap.example.jaxb.generated.pojos.GetBookRequest;
+import com.springboot.soap.example.jaxb.generated.pojos.GetBookResponse;
 import com.springboot.soap.example.repositories.BookRepository;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class BookEndpoint {
@@ -13,5 +18,12 @@ public class BookEndpoint {
         this.bookRepository = bookRepository;
     }
 
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getBookRequest")
+    @ResponsePayload
+    public GetBookResponse getCountry(@RequestPayload GetBookRequest request){
+        GetBookResponse response = new GetBookResponse();
+        response.setBook(bookRepository.findById(request.getId()));
+        return response;
+    }
 
 }
